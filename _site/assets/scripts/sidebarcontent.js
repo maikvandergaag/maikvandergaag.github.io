fec66cccdc661e8d68b9a9c57d83333a08703fd6
Maik van der Gaag <maikvandergaag@3fifty.eu>
2023-11-02 20:30:39 +0100

    updated git ignore

--- _site/assets/scripts/sidebarcontent.js
+++
@@ -1,5 +0,0 @@
-var divs = document.querySelectorAll('.sidebar');
-
-for (var i = 0; i < divs.length; i++) {
-    divs[i].innerHTML += '<iframe src="https://github.com/sponsors/maikvandergaag/button" title="Sponsor maikvandergaag" height="32" width="114" style="border: 0; border-radius: 6px;"></iframe>';
-}
